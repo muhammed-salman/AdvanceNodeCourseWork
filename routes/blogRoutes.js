6697d9cf2134d9d35dc6d5a04719cfaebc1e32a2
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2020-07-05 15:48:16 +0530

    Initial caching mechanism

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,27 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const redis = require('redis');
+    const redisURL = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisURL);
+    const util = require('util');
+    // returns promisified func for the supplied func, so that you do not need to use callbacks 
+    client.get = util.promisify(client.get);
 
+    //checking cached data available w.r.t query / request
+    const cachedBlogs = await client.get(req.user.id);
+    
+    //if cached data found sent it
+    if(cachedBlogs){
+      console.log('Serving from CACHE');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    //if cached data not found then send & cache it
+    const blogs = await Blog.find({ _user: req.user.id });
+    console.log('Serving from MongoDB');
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
