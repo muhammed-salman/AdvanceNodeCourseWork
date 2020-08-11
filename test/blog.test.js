50cc0c1945493ecf872b861d1c2b73debaa7b5ee
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2020-08-11 17:50:20 +0530

    Refactored blog.test

--- test/blog.test.js
+++ test/blog.test.js
@@ -67,36 +67,25 @@ describe('when logged in', () => {
 
 describe('When not logged in', () => {
     
-    test('User cannot create blog post', async () => {
-
-        const result = await page.evaluate(() => {
-                return fetch('/api/blogs', {
-                    method: 'POST',
-                    credentials: 'same-origin',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({title: 'My Title', content: 'My Content'})
-                }).then(res => res.json());
+    const actions = [
+        {
+            method: 'get',
+            path: '/api/blogs'
+        },
+        {
+            method: 'post',
+            path: '/api/blogs',
+            data: {
+                title: 'T',
+                content: 'C'
             }
-        );
+        }
+    ];
 
-        expect(result).toEqual({ error: 'You must log in!' });
-    });
-
-    test('User cannot get a list of blog post', async () => {
-
-        const result = await page.evaluate(() => {
-                return fetch('/api/blogs', {
-                    method: 'GET',
-                    credentials: 'same-origin',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).then(res => res.json());
-            }
-        );
-        
-        expect(result).toEqual({ error: 'You must log in!' });
+    test('Blog related actions are prohibited', async () => {
+        const results = await page.execRequests(actions);
+        for (let result of results){
+            expect(result).toEqual({error: 'You must log in!'});
+        }
     });
 });
