761dd2b000f1cd17117d9ffd9667797aa34a444d
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2020-08-11 22:57:27 +0530

    Added: config files for travis ci (sensitive info removed)

--- config/dev.js
+++ config/dev.js
@@ -4,4 +4,5 @@ module.exports = {
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
   cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
