82689fc0f772a2a6d569ea8beabe8a57a04fecf5
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2020-08-15 22:12:02 +0530

    Added: Aws fake config keys

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'ADSFDGFFQWRGTHHNWQQQI',
+  secretAccessKey: 'Upbghgghg2000vvgflnfdDGGGwdfg/jhghjy/' 
 };
