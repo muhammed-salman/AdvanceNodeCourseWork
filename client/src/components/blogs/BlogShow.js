f01742917a86eeb80f1d3d608e71d753887e87b0
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2020-08-15 22:16:17 +0530

    Render image on BlogShow page

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -7,6 +7,13 @@ class BlogShow extends Component {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    if (this.props.bog.imageUrl){
+      return <image src={
+        'https://my-blog-bucket-123.s3.amazonaws.com/' + this.props.blog.imageUrl} />;
+    }
+  }
+
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +25,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage}
       </div>
     );
   }
